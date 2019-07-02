76c89f83b98894547089c817f23a40c2923e2c2d
Wang <2210582727@qq.com>
2019-07-02 17:28:46 +0800

    2019-07-02  更新订单支付流程

--- application/extra/queue.php
+++ application/extra/queue.php
@@ -10,14 +10,14 @@
 // +----------------------------------------------------------------------
 
 return [
-    'connector'  => 'Redis',		    // Redis 驱动
-    'expire'     => 60,				// 任务的过期时间，默认为60秒; 若要禁用，则设置为 null
-    'default'    => 'default',		// 默认的队列名称
-    'host'       => '127.0.0.1',	    // redis 主机ip
-    'port'       => 6379,			// redis 端口
-    'password'   => 'wang911017',				// redis 密码
-    'select'     => 0,				// 使用哪一个 db，默认为 db0
-    'timeout'    => 0,				// redis连接的超时时间
-    'persistent' => false,			// 是否是长连接
-    //    'connector'   => 'Sync',		// Sync 驱动，该驱动的实际作用是取消消息队列，还原为同步执行
+//    'connector'  => 'Redis',		    // Redis 驱动
+//    'expire'     => 60,				// 任务的过期时间，默认为60秒; 若要禁用，则设置为 null
+//    'default'    => 'default',		// 默认的队列名称
+//    'host'       => '127.0.0.1',	    // redis 主机ip
+//    'port'       => 6379,			// redis 端口
+//    'password'   => 'wang911017',				// redis 密码
+//    'select'     => 0,				// 使用哪一个 db，默认为 db0
+//    'timeout'    => 0,				// redis连接的超时时间
+//    'persistent' => false,			// 是否是长连接
+        'connector'   => 'Sync',		// Sync 驱动，该驱动的实际作用是取消消息队列，还原为同步执行
 ];
