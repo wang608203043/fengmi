92d2a72c2a73c19196e55131b9a56d92791cc9f4
Wang <2210582727@qq.com>
2019-07-14 13:06:55 +0800

    2019-07-14

--- application/common/service/OrderService.php
+++ application/common/service/OrderService.php
@@ -365,7 +365,7 @@ class OrderService extends BaseService
             $data = [];
             $orders = (new Order())->with('goodsStock')->where(['auth_id'=>$auth_id,'status'=>$status])
                 ->page($page,3)->select();
-            halt($orders);
+            halt(json_decode(json_encode($orders)));
             foreach ($orders as $order) {
                 $order_goods = $order->goodsStock;
                 $goods_list = [];
